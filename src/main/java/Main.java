434fb28ba06d30b3da0e1871a1c267be57598ac5
Danil-Zaichkin <baziliopet@yandex.ru>
2022-11-12 20:18:52 +0500

    add TelegramBot class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 import bot.ConsoleBot;
+import bot.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleBot bot = new ConsoleBot();
-        bot.run();
+        //ConsoleBot bot = new ConsoleBot();
+        //bot.run();
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
