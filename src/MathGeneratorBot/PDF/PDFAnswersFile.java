9a7adaf4e77150f92c528dfc3e1ec07c4d012b82
Danil-Zaichkin <baziliopet@yandex.ru>
2022-12-15 20:00:10 +0500

    update file closing in pdf-generator

--- src/MathGeneratorBot/PDF/PDFAnswersFile.java
+++ src/MathGeneratorBot/PDF/PDFAnswersFile.java
@@ -14,17 +14,20 @@ public class PDFAnswersFile {
 
     public void generate(ArrayList<String> answers) throws IOException, DocumentException{
         Document document = new Document();
-
+        try {
         PdfWriter.getInstance(document, new FileOutputStream(filePath));
 
         document.open();
         Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-        Paragraph paragraph;
-        for (String answer : answers) {
-            paragraph = new Paragraph(answer, font);
-            document.add(paragraph);
+
+            Paragraph paragraph;
+            for (String answer : answers) {
+                paragraph = new Paragraph(answer, font);
+                document.add(paragraph);
+            }
+        } finally {
+            document.close();
         }
-        document.close();
     }
 
     public void setFilePath(String filePath) {
@@ -33,17 +36,5 @@ public class PDFAnswersFile {
 
     public String getPath() {
         return filePath;
-    }
-
-    public static void main(String[] args) throws IOException, DocumentException {
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
-
-        document.open();
-        Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.RED);
-        Paragraph paragraph = new Paragraph("Hello\n World", font);
-
-        document.add(paragraph);
-        document.close();
     }
 }
